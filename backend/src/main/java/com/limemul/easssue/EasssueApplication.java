2749c46ef6ce0a4e1d1c9d6473e1f8491d5e6b86
zaq1290 <zaq1290@naver.com>
2022-10-20 17:24:44 +0900

    backend project upload

---
+++ backend/src/main/java/com/limemul/easssue/EasssueApplication.java
@@ -0,0 +1,13 @@
+package com.limemul.easssue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasssueApplication.class, args);
+	}
+
+}
