58c7992e3be6e933a5c694515fec95fbcb01d2a0
eomsteve <mingyov0807@gmail.com>
2022-10-28 17:37:02 +0900

    feat : google auth need and .env

--- frontend/src/pages/newTab/newTab.tsx
+++ frontend/src/pages/newTab/newTab.tsx
@@ -7,11 +7,10 @@ import { Searchbar } from '@/components/Searchbar';
 import { NewsBoard } from '@/components/NewsBoard';
 import { DashIndex } from '@/components/Modals/DashBoard';
 import { PersistGate } from 'redux-persist/integration/react';
-import storage from 'redux-persist/lib/storage';
 import { Provider, useSelector } from 'react-redux';
 import { persistor, store } from '@/modules/store';
-
 const App: React.FC<{}> = () => {
+
   return (
     <div
       className="flex justify-center bg-cover"
@@ -28,6 +27,7 @@ const App: React.FC<{}> = () => {
         <KeywordModal />
         <DashIndex/>
       </div>
+      
     </div>
   );
 };
