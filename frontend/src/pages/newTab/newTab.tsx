715eed7685eebd734dee68270cffdaab29c2441c
eomsteve <mingyov0807@gmail.com>
2022-10-31 18:02:33 +0900

    feat: google oauth

--- frontend/src/pages/newTab/newTab.tsx
+++ frontend/src/pages/newTab/newTab.tsx
@@ -1,30 +1,30 @@
-import React, { useEffect, useState } from "react";
-import { createRoot } from "react-dom/client";
-import KeywordModal from "@/components/Modals/Keywords/KeywordComponent";
-import { BookmarkModal } from "@/components/BookmarkModal";
-import "@root/index.css";
-import { Bookmark } from "@/components/Bookmark";
-import { Searchbar } from "@/components/Searchbar";
-import { Settingbar } from "@/components/Settingbar";
-import { NewsBoard } from "@/components/NewsBoard";
-import { RealtimeKeyword } from "@/components/RealtimeKeyword";
+import React, { useEffect, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import KeywordModal from '@/components/Modals/Keywords/KeywordComponent';
+import { BookmarkModal } from '@/components/BookmarkModal';
+import '@root/index.css';
+import { Bookmark } from '@/components/Bookmark';
+import { Searchbar } from '@/components/Searchbar';
+import { Settingbar } from '@/components/Settingbar';
+import { NewsBoard } from '@/components/NewsBoard';
+import { RealtimeKeyword } from '@/components/RealtimeKeyword';
 import { DashIndex } from '@/components/Modals/DashBoard';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider, useSelector } from 'react-redux';
 import { persistor, store } from '@/modules/store';
-import Scrollbars from "react-custom-scrollbars-2";
-
+import Scrollbars from 'react-custom-scrollbars-2';
 
 const App: React.FC<{}> = () => {
   const [BookmarkModalOpen, setBookmarkModalOpen] = useState(false);
   const [bookmarkTree, setBookmarkTree] = useState<
     chrome.bookmarks.BookmarkTreeNode[]
   >([]);
+  const [myToken, setToken] = useState<string>('');
   useEffect(() => {
     fetchBookmarks();
   }, [BookmarkModalOpen]);
   const fetchBookmarks = () => {
-    chrome.bookmarks.getChildren("1", (bookmarkTreeNodes) => {
+    chrome.bookmarks.getChildren('1', (bookmarkTreeNodes) => {
       console.log(bookmarkTreeNodes);
       setBookmarkTree(bookmarkTreeNodes);
     });
@@ -34,9 +34,9 @@ const App: React.FC<{}> = () => {
       <div
         className="flex flex-col bg-cover"
         style={{
-          width: "100vw",
-          height: "100vh",
-          backgroundImage: "url(tab-background.jpg)",
+          width: '100vw',
+          height: '100vh',
+          backgroundImage: 'url(tab-background.jpg)',
         }}
       >
         <div className="h-8 p-2">
@@ -69,15 +69,37 @@ const App: React.FC<{}> = () => {
           setBookmarkModalOpen={setBookmarkModalOpen}
         ></BookmarkModal>
       )}
+      {/* <button
+        onClick={() =>
+
+          chrome.identity.getAuthToken({ interactive: true }, resToken => {
+            alert(resToken)
+            setToken(()=> resToken)
+            console.log(myToken);
+            
+    })
+        }
+      >
+        구글로그인  
+      </button>
+      <button onClick={()=>{
+        chrome.identity.removeCachedAuthToken(
+          {token: myToken}, function (){
+            console.log("remove");
+          }
+      );
+      }}>로그아웃</button> */}
     </>
   );
 };
 
-const container = document.createElement("div");
+const container = document.createElement('div');
 document.body.appendChild(container);
 const root = createRoot(container);
-root.render(<Provider store={store}>
-  <PersistGate loading={null} persistor={persistor}>
-    <App />
-  </PersistGate>
-</Provider>);
+root.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
