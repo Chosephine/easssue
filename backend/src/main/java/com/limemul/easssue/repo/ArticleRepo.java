a3eb966bc5f5ac80d529b671ac5198c524496653
Hyoeun <nktion@naver.com>
2022-11-01 17:50:14 +0900

    feat: modify popular article repo

--- backend/src/main/java/com/limemul/easssue/repo/ArticleRepo.java
+++ backend/src/main/java/com/limemul/easssue/repo/ArticleRepo.java
@@ -1,16 +1,18 @@
 package com.limemul.easssue.repo;
 
 import com.limemul.easssue.entity.Article;
+import com.limemul.easssue.entity.ArticleKwd;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 public interface ArticleRepo extends JpaRepository<Article, Long> {
 
     Slice<Article> findByPubDateAfterOrderByHitDesc(LocalDateTime pubDate, Pageable pageable);
 
-
+    Slice<Article> findAllByArticleKwdsIsNotInOrderByHitDesc(Collection<List<ArticleKwd>> banKwdList, Pageable pageable);
 }
