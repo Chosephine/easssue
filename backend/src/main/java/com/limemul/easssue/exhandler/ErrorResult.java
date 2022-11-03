6e1fd5741d85ebaf633fe5a161bd786fcb128aa8
zaq1290 <zaq1290@naver.com>
2022-11-03 09:50:27 +0900

    feat: add exception handler

---
+++ backend/src/main/java/com/limemul/easssue/exhandler/ErrorResult.java
@@ -0,0 +1,19 @@
+package com.limemul.easssue.exhandler;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class ErrorResult {
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+}
