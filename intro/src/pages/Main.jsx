1eb9dbfc040d2cf62b0404e82b54c478efd6c425
eomsteve <mingyov0807@gmail.com>
2022-11-16 16:33:34 +0900

    connect to extension page

--- intro/src/pages/Main.jsx
+++ intro/src/pages/Main.jsx
@@ -3,6 +3,9 @@ import { gsap, Back } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger'; //https://www.youtube.com/watch?v=_-_JCocqNbw
 const Main = () => {
   useEffect(() => {}, []);
+  const gotoExtension = () =>{
+    return window.location.href="https://chrome.google.com/webstore/detail/%EC%9E%88%EC%8A%88-easssue/cmnmdjpabceejnbkdlijepkmcdpdohjl?hl=ko&"
+  }
   return (
     <>
       <section className="section02 scale-125 panel">
@@ -11,15 +14,15 @@ const Main = () => {
 
           <div className="flex justify-center w-full flex-col mr-[4rem] font-service">
             <div className="text-[1.5rem] border-gray-400 border-b-2">
-              책상앞에서 세상을 알다마다요
+              책상앞에서 세상을 알다.
             </div>
             <div className="font-title h-full flex justify-center items-end text-[6rem] pb-2">
               <div>{'있슈'}</div> <div className='text-[4rem]'>easssue</div>
             </div>
           </div>
-          <img className="logo w-[180px] h-[180px] hover:cursor-pointer" src="biglogo.png" alt="" />
+          <img onClick={gotoExtension} className="logo w-[180px] h-[180px] hover:cursor-pointer" src="biglogo.png" alt="" />
           </div>
-          <div className='text-end mr-[-2px] text-blue-400 hover:cursor-pointer'>
+          <div onClick={gotoExtension} className='text-end mr-[-2px] text-blue-400 hover:cursor-pointer'>
             확장프로그램 설치하기 ↗
           </div>
           {/* <div className='mt-3 flex'>
