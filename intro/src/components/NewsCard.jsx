036294cdc9d4faac7a1c0a819c89144b4048f617
엄성현 <mingyov0807@gmail.com>
2022-11-17 23:56:45 +0900

    feat: intro-news components

--- intro/src/components/NewsCard.jsx
+++ intro/src/components/NewsCard.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
 
-const NewsCard = ({bgColor}) =>{
-  return (<>
-  
-  </>)
-}
+const NewsCard = ({ bgColor }) => {
+  bgColor = "123"
+  return (
+    <>
+      <div className="flex flex-col justify-between rounded-xl h-full bg-white/75 relative">
+        <a target="_blank">
+          <img
+            className="rounded-t-xl w-full h-[180px] object-cover object-center mb-2"
+            src=""
+            alt=""
+          />
+          <div className="flex flex-col justify-between bg-black/75 rounded-t-xl w-full h-[180px] absolute top-0 p-2">
+            <div className="text-white text-xl text-ellipsis line-clamp-4 m-2"></div>
+          </div>
+
+          <div className=" text-xl mx-2 font-bold text-ellipsis line-clamp-2 h-[56px]">
+            {'타이틀'}
+          </div>
+        </a>
+        <div>
+          <div className="text-sm mx-2 text-right">{'1시간 전'}</div>
+          <div className="grid grid-cols-3">
+            <div className="mx-1">
+              <div
+                className="bg-black/50 text-[0.875rem] rounded-full h-8 text-white p-1 text-center my-2 justify-center text-ellipsis line-clamp-1"
+              >
+                {'키워드'}
+              </div>
+            </div>
+        
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default NewsCard;
--- intro/src/pages/News.jsx
+++ intro/src/pages/News.jsx
@@ -5,12 +5,12 @@ const News = () => {
   return (
     <>
     <section className="panel">
-      <div className='dash flex w-[100%] justify-center items-center'>
-        <div className='news-container h-[40vh] w-[30vw] flex flex-wrap border-2 border-black'>
-          {colors.map((color, index)=>{
-            // <NewsCard bgColor={color}></NewsCard>
-          })}
-
+      <div className='news-board flex w-[100%] justify-center items-center'>
+        <div className='news-container h-[50vh] w-[40vw] grid grid-cols-2 gap-4 bg-black/25 rounded-lg p-4 border-2 border-black'>
+        <NewsCard/>
+        <NewsCard/>
+        <NewsCard/>
+        <NewsCard/>
         </div>
         <div className='news-p ml-10 text-[1.4rem] font-service'>
           와이티엔~
