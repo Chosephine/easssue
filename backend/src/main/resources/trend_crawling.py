edda82870594d790a35ef8d7ac956b26bcf9773b
zaq1290 <zaq1290@naver.com>
2022-11-17 17:18:58 +0900

    fix: rollback trend crawling (encoding error)

--- backend/src/main/resources/trend_crawling.py
+++ backend/src/main/resources/trend_crawling.py
@@ -5,7 +5,7 @@ import json
 url = 'https://www.nate.com/js/data/jsonLiveKeywordDataV1.js?v=202211150050'
 response = requests.get(url)
 
-soup = BeautifulSoup(response.text,"lxml", from_encoding="cp949")
+soup = BeautifulSoup(response.text,"lxml")
 k = soup.p.string
 x = json.loads(k)
 for i in range(10):
