05e0f9fb6f92c308534b120e38178bd2c659bd1c
eomsteve <mingyov0807@gmail.com>
2022-11-17 17:44:52 +0900

    bug fix font-bold

--- intro/src/components/Popup.jsx
+++ intro/src/components/Popup.jsx
@@ -18,22 +18,22 @@ const PopUpComponent = ({ toggle }) => {
         <div className="flex flex-col m-0  w-[100%] h-[550px]">
           <div className="p-3 w-[100%] flex items-center bg-blue-100">
             <img className="w-10 h-10 " src="biglogo.png" alt="" />
-            <h1 className="ml-3 text-xl font-bold">easssue 있슈</h1>
+            <h1 className="ml-3 text-xl  ">easssue 있슈</h1>
           </div>
           {isLoading ? (
             <div
               className={`h-[500px] flex justify-center flex-col items-center`}
             >
               <img className="ml-3 h-[30%] w-[30%]" src="spinner.gif" alt="" />
-              <h1 className="ml-3 text-xl font-bold">기사 요약중...</h1>
+              <h1 className="ml-3 text-xl font-[700]">기사 요약중...</h1>
             </div>
           ) : (
             <div>
               <div className="mt-1 px-3 w-[100%] flex items-center justify-end">
-                <h1 className="ml-3 text-md text-slate-600 font-bold truncate">{`있슈 easssue!`}</h1>
+                <h1 className="ml-3 text-md text-slate-600 font-[700] truncate">{`있슈 easssue!`}</h1>
               </div>
               <div className="w-[100%] flex items-center">
-                <h2 className="m-3 text-lg font-bold border-b-2 border-gray-400">
+                <h2 className="m-3 text-lg font-[700] border-b-2 border-gray-400">
                   키워드 클라우드
                 </h2>
               </div>
@@ -43,7 +43,7 @@ const PopUpComponent = ({ toggle }) => {
                 alt="keyword cloud"
               />
               <div className="w-[100%] flex items-center">
-                <h2 className="m-3 text-lg font-bold border-b-2 border-gray-400">
+                <h2 className="m-3 text-lg font-[700] border-b-2 border-gray-400">
                   기사 요약
                 </h2>
               </div>
